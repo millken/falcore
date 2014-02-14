f0e02953233aa6d474ba0514d9b262e3091629ae
Dave Grijalva <grijalva@gmail.com>
2014-02-14 00:50:28 -0800

    cleaned up upstream_throttle_test to use channels instead of sleep.  much faster

--- filter/upstream_throttle_test.go
+++ filter/upstream_throttle_test.go
@@ -1,22 +1,34 @@
 package filter
 
 import (
+	"fmt"
 	"github.com/fitstar/falcore"
-	"math"
 	"net/http"
+	// "strconv"
 	"testing"
-	"time"
-	// "fmt"
 )
 
+const REQ_COUNT = 100
+
 func TestUpstreamThrottle(t *testing.T) {
+	// Build a thing for
+	var started = make(chan chan bool, REQ_COUNT+1)
+
 	// Start a test server
 	sleepPipe := falcore.NewPipeline()
 	sleepPipe.Upstream.PushBack(falcore.NewRequestFilter(func(req *falcore.Request) *http.Response {
-		time.Sleep(time.Second)
+		// get chan
+		// j, _ := strconv.Atoi(req.HttpRequest.URL.Query().Get("j"))
+		// fmt.Println(req.HttpRequest.URL, j)
+		c := make(chan bool)
+		started <- c
+		// wait on chan
+		<-c
+		// fmt.Println("DONE")
 		return falcore.StringResponse(req.HttpRequest, 200, nil, "OK")
 	}))
 	sleepSrv := falcore.NewServer(0, sleepPipe)
+	defer sleepSrv.StopAccepting()
 	go func() {
 		sleepSrv.ListenAndServe()
 	}()
@@ -27,33 +39,39 @@ func TestUpstreamThrottle(t *testing.T) {
 	// pipe := falcore.NewPipeline()
 	// pipe.Upstream.PushBack(up)
 
-	resCh := make(chan *http.Response, 10)
+	resCh := make(chan *http.Response, REQ_COUNT)
 	var i int64 = 1
 	for ; i < 12; i++ {
-		start := time.Now()
+		// fmt.Println("Testing with limit", i)
+
 		up.SetMaxConcurrent(i)
 
-		for j := 0; j < 10; j++ {
+		for j := 0; j < REQ_COUNT; j++ {
+			var jj = j
 			go func() {
-				req, _ := http.NewRequest("GET", "http://localhost/", nil)
+				// fmt.Println("STARTING")
+				req, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost/foo?j=%v", jj), nil)
 				_, res := falcore.TestWithRequest(req, up, nil)
+				res.Body.Close()
 				resCh <- res
 				// fmt.Println("OK")
 			}()
 		}
-		for j := 0; j < 10; j++ {
+		for j := 0; j < REQ_COUNT; j++ {
+			// make sure we haven't gone over the limit
+			// fmt.Println(i, len(started))
+			if r := int64(len(started)); r > i {
+				t.Errorf("%v: Over the limit: %v", i, r)
+			}
+
+			// send a finish signal
+			(<-started) <- true
+
+			// collect the result
 			res := <-resCh
 			if res.StatusCode != 200 {
 				t.Fatalf("Error: %v", res)
 			}
-		}
-
-		duration := time.Since(start)
-		seconds := float64(duration) / float64(time.Second)
-		goal := math.Ceil(10.0 / float64(i))
-		// fmt.Println(i, "Time:", seconds, "Goal:", goal)
-		if seconds < goal {
-			t.Errorf("%v: Too short: %v < %v", i, seconds, goal)
 		}
 	}
 
